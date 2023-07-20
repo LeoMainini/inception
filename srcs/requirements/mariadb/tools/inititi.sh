b62284314d70741f619ee97b3a54a2ba236a4d0d
leferrei <leferrei@student.42lisboa.com>
2023-07-20 19:56:19 +0100

    fix db init script to build file on recurring build with init done

--- srcs/requirements/mariadb/tools/inititi.sh
+++ srcs/requirements/mariadb/tools/inititi.sh
@@ -3,9 +3,9 @@ chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 if [ ! -f /var/lib/mysql/.initialized ]; then
 	echo "initializing mariadb\n"
 	mariadb-install-db --user=mysql --ldata=/var/lib/mysql --defaults-file=/etc/mysql/my.cnf 2>> /var/lib/mysql/loginstall.out 1>> /var/lib/mysql/loginstall.out
-	sh build.sh
 	touch /var/lib/mysql/.initialized
 fi
-
 echo "\nmariadb initialized\n"
+
+sh build.sh
 exec "$@"
